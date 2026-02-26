65d1714a94edfe02242cc080436e210c31f179dd
cvHanky <danielhoppe123@gmail.com>
2026-02-26 10:33:53 +0100

    When an item is collapsed, its children now also automatically collapse. Added subtle borders to items to make it feel a bit more graspable, and also improved the animations a bit.

--- Dynamic search for hierarchic graph/HierarchicGraph.Database/Repositories/GraphItemRepository.cs
+++ Dynamic search for hierarchic graph/HierarchicGraph.Database/Repositories/GraphItemRepository.cs
@@ -16,9 +16,9 @@ public class GraphItemRepository
         context = dbContext;
     }
 
-    public async Task<GraphItem?> GetItemById(int id)
+    public GraphItem? GetItemById(int id)
     {
-        return await context.Items.FindAsync(id);
+        return context.Items.Find(id);
     }
 
     public List<GraphItem> GetAll()
@@ -38,7 +38,38 @@ public class GraphItemRepository
         return true;
     }
 
-    public List<GraphItem> GetSortedItems()
+    /// <summary>
+    /// Returns a list of ids of all descendants of the item with <paramref name="itemId"/>.
+    /// </summary>
+    /// <param name="itemId"></param>
+    /// <returns></returns>
+    public List<int> GetAllDescendants(int itemId)
+    {
+        var item = GetItemById(itemId);
+        var result = new List<int>();
+
+        if (item is null)
+        {
+            // Theoretically there should be some logging here, not necessary for the scope of this project.
+            return result;
+        }
+
+        var childList = new List<GraphItem>();
+        CollectDescendantsRecursively(item, sortedList: childList, fullList: GetAll());
+
+        foreach (var child in childList )
+        {
+            result.Add(child.Id);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Returns a sorted list of a full heritage tree of <see cref="GraphItem"/>. Can optionally be sorted alphabetically (not implemented yet).
+    /// </summary>
+    /// <returns></returns>
+    public List<GraphItem> GetSortedItems(bool sortAlphabetically = false)
     {
         List<GraphItem> sortedList = new List<GraphItem>();
         List<GraphItem> fullList = GetAll().ToList();
@@ -46,22 +77,34 @@ public class GraphItemRepository
         var rootItems = fullList.Where(i => i.ParentId is null);
         foreach (var rootItem in rootItems)
         {
-            CollectDescendantsRecursively(rootItem, sortedList, fullList, 0);
+            CollectDescendantsRecursively(rootItem, sortedList, fullList, setDepth: true, depth: 0);
         }
 
         return sortedList;
     }
 
-    private void CollectDescendantsRecursively(GraphItem item, List<GraphItem> sortedList, List<GraphItem> fullList, int depth)
+    /// <summary>
+    /// Given a list <paramref name="sortedList"/>, adds all descendants of <paramref name="item"/> to that list. 
+    /// Optionally sets the <paramref name="depth"/> of all items if <paramref name="setDepth"/> is true.
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="sortedList"></param>
+    /// <param name="fullList"></param>
+    /// <param name="setDepth"></param>
+    /// <param name="depth"></param>
+    private static void CollectDescendantsRecursively(GraphItem item, List<GraphItem> sortedList, List<GraphItem> fullList, bool setDepth = false, int depth = 0)
     {
-        item.Depth = depth; // every time the recursion goes a layer deeper, depth is incremented by 1.
+        if (setDepth)
+        {
+            item.Depth = depth; // every time the recursion goes a layer deeper, depth is incremented by 1.
+        }
         sortedList.Add(item);
 
         if (fullList.Where(i => i.ParentId == item.Id).ToList() is List<GraphItem> children && children.Any()) // Figures out if the item has any children, and then also collects THEIR descendants.
         {
-            foreach(var child in children)
+            foreach (var child in children)
             {
-                CollectDescendantsRecursively(child, sortedList, fullList, depth+1);
+                CollectDescendantsRecursively(child, sortedList, fullList, setDepth:setDepth, depth:depth + 1);
             }
         }
     }
